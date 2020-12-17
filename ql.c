2cdeabda9111d9f3d70fde790c77f1388eec10cc
Skylar Broad <skylar.broad@gmail.com>
2020-12-16 16:40:46 -0800

    add parsing and printing

--- ql.c
+++ ql.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
 #define INBUF_SIZE 256
 char inbuf[INBUF_SIZE];
 
@@ -11,6 +12,20 @@ char *punctuation[] = {
 	"'"
 };
 
+int LP = 0;
+int RP = 1;
+int Q = 2;
+int QUOTE = 3;
+
+typedef struct _pair {
+	int hd;
+	int tl;
+} pair;
+
+pair *pairs = NULL;
+size_t npairs = 0;
+size_t mpairs = 0;
+
 char **symbols = NULL;
 size_t nsymbols = 0;
 size_t msymbols = 0;
@@ -18,6 +33,36 @@ size_t msymbols = 0;
 int *sym_list = NULL;
 size_t nsym_list = 0;
 size_t msym_list = 0;
+
+int cons(int hd, int tl) {
+	if (npairs == mpairs) {
+		int new_count = mpairs ? mpairs * 2 : 8;
+		if (!(pairs = realloc(pairs, new_count * sizeof(pair)))) {
+			perror("realloc");
+			exit(1);
+		}
+		mpairs = new_count;
+	}
+	pairs[npairs].hd = hd;
+	pairs[npairs].tl = tl;
+	return -++npairs;
+}
+
+int hd(int p) {
+	if (p < 0 && -1 - p < npairs) {
+		return pairs[-1 - p].hd;
+	}
+	// not a pair
+	return 0;
+}
+
+int tl(int p) {
+	if (p < 0 && -1 - p < npairs) {
+		return pairs[-1 - p].tl;
+	}
+	// not a pair
+	return 0;
+}
 
 int push_symbol(char *symbol) {
 	for (int i = 0; i < nsymbols; i++) {
@@ -109,10 +154,122 @@ void process_input() {
 	process_chunk(start + bytes_read, 1);
 }
 
-int main() {
-	process_input();
-	for (int i = 0; i < nsym_list; i++) {
-		printf("%s\n", symbols[sym_list[i]]);
+char *to_s(int);
+
+char *p_to_s(int e) {
+	if (e == 0) {
+		return strdup("");
+	} else if (e > 0) {
+		// " . " + to_s(e)
+		char *es = to_s(e);
+		int les = strlen(es);
+		int len = les + 4;
+		char *res = malloc(len);
+		res[0] = ' ';
+		res[1] = '.';
+		res[2] = ' ';
+		strcpy(res+3, es);
+		free(es);
+		return res;
+	} else {
+		// " " + to_s(hd(e)) + p_to_s(tl(e))
+		char *hds = to_s(hd(e));
+		char *tls = p_to_s(tl(e));
+		int lhds = strlen(hds);
+		int ltls = strlen(tls);
+		int len = 2 + lhds + ltls;
+		char *res = malloc(len);
+		res[0] = ' ';
+		strcpy(res+1, hds);
+		strcpy(res+1+lhds, tls);
+		free(hds);
+		free(tls);
+		return res;
 	}
+}
+
+char *to_s(int e) {
+	if (e == 0) {
+		return strdup("()");
+	} else if (e > 0) {
+		return strdup(symbols[e]);
+	} else {
+		// "(" + to_s(hd(e)) + p_to_s(tl(e)) + ")"
+		char *hds = to_s(hd(e));
+		char *tls = p_to_s(tl(e));
+		int lhds = strlen(hds);
+		int ltls = strlen(tls);
+		int len = 3 + lhds + ltls;
+		char *res = malloc(len);
+		res[0] = '(';
+		strcpy(res+1, hds);
+		strcpy(res+lhds+1, tls);
+		res[len-2] = ')';
+		res[len-1] = 0;
+		free(hds);
+		free(tls);
+		return res;
+	}
+}
+
+struct parse_t {
+	int value;
+	int next_token;
+};
+
+struct parse_t parse_lst(int next_token) {
+	struct parse_t r = {0, next_token + 1};
+	if (next_token >= nsym_list) {
+		perror("unmatched `(`");
+		exit(1);
+	}
+	int t = sym_list[next_token];
+	if (t == RP) {
+		return r;
+	}
+	if (t == LP) {
+		struct parse_t hd = parse_lst(next_token+1); 
+		struct parse_t tl = parse_lst(hd.next_token);
+		r.value = cons(hd.value, tl.value);
+		r.next_token = tl.next_token;
+		return r;
+	}
+	struct parse_t tl = parse_lst(next_token + 1);
+	r.value = cons(t, tl.value);
+	r.next_token = tl.next_token;
+	return r;
+}
+
+struct parse_t parse_exp(int next_token) {
+	struct parse_t r = {0, next_token + 1};
+	if (next_token >= nsym_list) {
+		r.next_token = -1;
+		return r;
+	}
+	int t = sym_list[next_token];
+	if (t == LP) return parse_lst(next_token+1);
+	if (t == RP) {
+		perror("unmatched `)`");
+		exit(1);
+	}
+	r.value = t;
+	return r;
+}
+
+void init_symbols() {
+	for (int p = 0; p < sizeof(punctuation)/sizeof(char *); p++)
+		push_symbol(punctuation[p]);
+	push_symbol("quote");
+}
+
+int main() {
+	init_symbols();
+
+	process_input();
+
+	for (struct parse_t p = parse_exp(0); p.next_token != -1; p = parse_exp(p.next_token)) {
+		printf("%s\n", to_s(p.value));
+	}
+
 	return 0;
 }
